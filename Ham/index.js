d1a2486e64604e4b159ebe6862045d180ba30d60
vulinh1704 <truongdangvulinh1704@gmail.com>
2024-07-29 20:26:01 +0700

    quản lý sản phẩm

--- Ham/index.js
+++ Ham/index.js
@@ -91,19 +91,163 @@ onInput: Có bất cứ thay nào thì thực hiện hàm luôn
 ...
 */
 
-function showHello() {
-    alert("XIn chào các bé")
+// function showHello() {
+//     alert("XIn chào các bé")
+// }
+
+
+// function showName() {
+//     let name = document.getElementById("name").value;
+//     console.log("Name: " + name);
+// }
+
+/*
+function isPrime(number) {
+    let count = 0
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            count++;
+        }
+    }
+    if (count === 2) {
+        alert(number + " là số nguyên tố")
+    } else {
+        alert(number + " k là số nguyên tố")
+    }
+}
+
+let a = prompt("Nhập số cần kiểm tra: ");
+isPrime(a);
+*/
+
+// CamelCase 
+// Feet: tham số truyền vào, sử dụng khi khai báo
+// Đối số: đối số là giá trị thực truyền vào hàm khi gọi hàm (số lượng ~ số lượng tham số truyền vào)
+// return: trả về giá trị cho hàm, khi gặp từ khóa return ngay lập tức dừng hàm và trả về giá trị
+
+/*
+function feetToMeter(feet){ // Khai báo hàm
+    let meter = 0.305 * feet;
+    return meter;
+}
+
+function meterToFeet(meter) {
+    let feet = 3.279 * meter;
+    return meter;
+}
+
+let feet = prompt("Nhập feet: ")
+let meter = feetToMeter(feet); // GỌi hàm
+alert("Meter: " + meter)
+*/
+
+// Sự kiện: onchange, onclick, oninput
+
+
+/* 
+Có 2 phạm vi của biến:
++ Biến toàn cục: Biến được khai báo ngoài hàm, khối. Tồn tại từ lúc khai báo đến hết chương trình.
++ Biến cục bộ: Biến cục bộ là biến được khai báo trong hàm hoặc khối. TỒn tại từ lúc khai báo đến hết khối hoặc hàm
+Note: Chỉ cần khác nhau về phạm vi có thể trùng tên biến
+
+let a = 10; // Biến toàn cục
+if (true) {
+    let a = 20; // Biến cục bộ 
+    alert(a); // Lấy biến gần nhất
+}
+
+for (let i = 0; i < 10; i++) { // Biến cục bộ
+    console.log(i);
+}
+
+function go(name) {
+    let b = 10;
+}
+
+let c = 21;
+
+*/
+
+
+/*
+Phân biệt let,var:
+- Giống: Khai báo biến trong Js
+- Khác:
+   + Cơ chế Hosting:
+   let: Không thể sử dụng biến trước khi khai báo.
+   var: Có thể sử dụng biến trước khi báo.
+
+   + let: Không thể tạo lại biến cùng tên, chỉ có thể gán lại
+     var: có thể tạo lại biến cùng tên.
+
+   + Phạm vi khai báo
+   let: Phạm vi là block scope(Khối)
+   var: Phạm vi là function scope (hàm)
+
+   const ~ let: Khác ở chỗ const không cho phép gán lại giá trị
+*/
+
+
+
+/*
+
+function go() {
+    var b = 20;
+    if(true) {
+        var a = 10;
+    }
+    console.log("A block scope", a);
+}
+console.log(a, b);
+
+go();
+*/
+/*
+function show20Prime() {
+    let number = 1;
+    let countPrime = 0;
+    while (countPrime <= 20) {
+        let count = 0
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) {
+                count++;
+            }
+        }
+        if (count === 2) {
+           console.log(number);
+           countPrime++;
+        }
+        number++; 
+    }
 }
 
 
-function showName() {
-    let name = document.getElementById("name").value;
-    console.log("Name: " + name);
+show20Prime();
+*/
+
+/*
+let englishs = ["Hello", "Banana", "Cat", "Dog", "Cake"] // 3
+let vietNameses = ["Xin chào", "Chuối", "Mèo", "Chó", "Bánh"]
+// => Xây dựng 1 hàm nhập tiếng Anh => Tiếng Việt
+// Ý tưởng: tìm vị trí của từ đó trong mảng 1 và in từ tương ứng ở mảng 2
+
+function translate(word) {
+    let index;
+    for (let i = 0; i < englishs.length; i++) {
+        if(englishs[i] === word) {
+            index = i;
+            break;
+        }    
+    }
+    
+    let ouput = vietNameses[index];
+    return ouput;
 }
 
-
-
-
+let input = prompt("Nhập từ tiếng Anh: ");
+let a = translate(input);
+alert(a);
+*/
 
 
 
---
+++ QuanLySanPham/index.js
@@ -0,0 +1,65 @@
+
+
+
+/*
+Các bước làm 1 tính năng:
+B1: Xây dựng HTML
+B2: Xây dựng hàm bên Js
+B3: Xác định phần tử HTML mà mình muốn tương tác
+*/
+
+let list = ["Iphone", "Samsung", "Iphone 11"];
+
+// Tìm kiếm gần đúng, tìm kiếm
+// Tìm hiểu thêm về local storage
+function addProduct() {
+    let newProduct = document.getElementById("new-product").value;
+    list.push(newProduct);
+    showAll();
+    alert("Thêm thành công");
+    document.getElementById("new-product").value = '';
+}
+
+function remove(index) {
+    let isConfirm = confirm("Are you sure?");
+    if (isConfirm) {
+        list.splice(index, 1);
+        showAll();
+        alert("Xóa thành công");
+    }
+}
+
+function showFormEdit(index) {
+    document.getElementById("new-product").value = list[index];
+    document.getElementById('btn').innerHTML = `
+    <button onclick="edit(${index})">Save</button>
+    `
+}
+
+function edit(index) {
+    let newProduct = document.getElementById("new-product").value;
+    list[index] = newProduct;
+    alert("Sửa thành công");
+    showAll();
+    document.getElementById("new-product").value = '';
+    document.getElementById('btn').innerHTML = `
+     <button onclick="addProduct()">Add</button>
+    `
+}
+
+function showAll() {
+    let html = '';
+    for (let i = 0; i < list.length; i++) {
+        html += `
+            <tr>
+                <td>${list[i]}</td>
+                <td><button onclick="showFormEdit(${i})">Sửa</button></td>
+                <td><button onclick="remove(${i})">Xóa</button></td>
+            </tr>
+        `;
+    }
+    document.getElementById('list-product').innerHTML = html;
+}
+
+showAll();
+// Create, Read ,Update, Delete 
